c62d98ed2d79e044a828d0ced34a610c675f202d
Pedro <pedromoratelli@gmail.com>
2019-11-27 20:59:20 -0300

    Adicionar opções de 'Raio' e 'Destino' em 'Achar Carona'

--- src/public/scripts/achar-carona.js
+++ src/public/scripts/achar-carona.js
@@ -21,10 +21,58 @@ $(document).ready(async () => {
 
   await Swal.fire({
     html: `
-      <input id="campoDistancia">
-      <select id="campoInstituicao">
-        <option value="2">Senai</option>
-      </select>
+      <div class="alertContainer">
+        <h6 class="mdc-typography--headline6">Achar Carona</h6>
+        
+        <p class="mdc-typography--overline">RAIO DE BUSCA (EM METROS)</p>
+        <div class="mdc-slider mdc-slider--discrete" data-mdc-auto-init="MDCSlider" tabindex="0" role="slider"
+        aria-valuemin="0" aria-valuemax="100" aria-valuenow="0" data-step="10" aria-label="Select Value" style="padding: 0; margin: 0">
+            <div class="mdc-slider__track-container">
+             <div class="mdc-slider__track"></div>
+            </div>
+            <div class="mdc-slider__thumb-container">
+              <div class="mdc-slider__pin">
+                <span class="mdc-slider__pin-value-marker"></span>
+              </div>
+            <svg class="mdc-slider__thumb" width="21" height="21">
+              <circle cx="10.5" cy="10.5" r="7.875"></circle>
+            </svg>
+            <div class="mdc-slider__focus-ring"></div>
+          </div>
+        </div>
+       
+        <p class="mdc-typography--overline">DESTINO</p>
+        <div class="mdc-select mdc-select--outlined mdc-select--required" data-mdc-auto-init="MDCSelect" style="width: 100%">
+          <input
+            id="campoInstituicao"
+            type="hidden"
+            name="enhanced-select"
+          />
+          <i class="mdc-select__dropdown-icon"></i>
+          <div
+            class="mdc-select__selected-text"
+            aria-required="true"
+          ></div>
+          <div
+            class="mdc-select__menu mdc-menu mdc-menu-surface demo-width-class"
+          >
+            <ul class="mdc-list" id="listaInstituicoes">
+              <li
+                class="mdc-list-item mdc-list-item--selected"
+                data-value=""
+                aria-selected="true"
+              ></li>
+            </ul>
+          </div>
+          <div class="mdc-notched-outline">
+            <div class="mdc-notched-outline__leading"></div>
+            <div class="mdc-notched-outline__notch">
+              <label class="mdc-floating-label">Instituição</label>
+            </div>
+            <div class="mdc-notched-outline__trailing"></div>
+          </div>
+        </div>
+      </div>
     `,
     confirmButtonText: "Buscar",
     showCancelButton: true,
@@ -36,20 +84,35 @@ $(document).ready(async () => {
     onClose: () => {
       location = "/suas-caronas.html";
     },
+    onOpen: () => {
+      mdc.autoInit();
+      /* const slider = mdc.slider.MDCSlider.attachTo(
+        document.querySelector(".mdc-slider")
+      ); */
+      $(window).resize(function() {
+        $(".mdc-select__menu").width($(".mdc-select").width());
+        /* slider.layout(); */
+      });
+
+      $(window).trigger("resize");
+    },
     preConfirm: () => {
       const instituicao = $("#campoInstituicao :selected").text();
       const id_instituicao = $("#campoInstituicao").val();
       distancia = $("#campoDistancia").val();
 
-      return fetch(`/buscar?id_instituicao=${id_instituicao}&distancia=${distancia}`, {
-        headers: {
-          Authorization: `Bearer ${localStorage.token}`
+      return fetch(
+        `/buscar?id_instituicao=${id_instituicao}&distancia=${distancia}`,
+        {
+          headers: {
+            Authorization: `Bearer ${localStorage.token}`
+          }
         }
-      })
+      )
         .then(async response => {
           const res = await response.json();
 
-          if(res.length === 0) {
+          if (res.length === 0) {
             throw new Error();
           }
 
