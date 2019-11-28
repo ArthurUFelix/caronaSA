cb8b4d9306e7f3605d119b78e5962488c17041a6
Pedro <pedromoratelli@gmail.com>
2019-11-27 21:34:23 -0300

    Editar como os Cards são criados em 'Suas Caronas'

--- src/public/scripts/suas-caronas.js
+++ src/public/scripts/suas-caronas.js
@@ -37,19 +37,25 @@ $(document).ready(async () => {
       <div class="cardWrapper mdc-elevation--z1">
         <div class="cardContainer">
           <h6 class="mdc-dialog__title">${carona.instituicao.nome}</h6>
+          <div class="mdc-switch mdc-switch--checked" data-mdc-auto-init="MDCSwitch">
+            <div class="mdc-switch__track"></div>
+            <div class="mdc-switch__thumb-underlay">
+              <div class="mdc-switch__thumb">
+                <input type="checkbox" id="basic-switch" class="mdc-switch__native-control" role="switch" checked>
+              </div>
+            </div>
+          </div>
           <p class="mdc-typography--body1">${carona.periodo}</p>
           <p class="mdc-typography--body1">${carona.dias}</p>
           <div class="linebreak"></div>
           <div class="mdc-card__actions">
-            <button class="mdc-button mdc-card__action mdc-card__action--button">
-              <span class="mdc-button__label">EDITAR</span>
-            </button>
             <button class="mdc-button mdc-card__action mdc-card__action--button">
               <span class="mdc-button__label">DELETAR</span>
             </button>
           </div>
         </div>
       </div>
+      <script type="text/javascript">mdc.autoInit();</script>
     `);
   });
 });
